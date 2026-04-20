371d581984f68e7039e1f17fc66ee1756c7d73f7
proimagedesign <contato.proimage@gmail.com>
2026-04-19 21:27:46 -0300

    fix: Scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Admin from "./pages/Admin";
 import AdminLogin from "./pages/AdminLogin";
 import NotFound from "./pages/NotFound";
 import { Chatbot } from "./components/Chatbot";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -23,6 +24,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/apresentacao" element={<Apresentacao />} />
